ac11f83275966c32d07cf8952a0978bf71837d81
Claudio Postinghel <claudiopostinghel@gmail.com>
2021-02-05 22:25:04 +0100

    Hey

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,58 @@
 <script>
+
+	// Import
+	import { onMount } from 'svelte';
+
+	// Export
 	export let name;
+
+	// Variables
+	var myCanvas;
+	var myImage;
+
+	// On mount
+	onMount(() => {
+
+		// Trovo gli elementi
+		myCanvas = document.getElementById("myCanvas");
+		myImage = document.getElementById("myImage");
+
+		// Disegno la canvas
+		var ctx = myCanvas.getContext("2d");
+
+		var img = new Image();
+		img.onload = function() {
+			ctx.drawImage(img, 0, 0);
+		}
+		img.src = "http://upload.wikimedia.org/wikipedia/commons/d/d2/Svg_example_square.svg";
+
+	});
+
+	// Converts canvas to image
+	function display() {
+		const dataURI = myCanvas.toDataURL();
+		
+		myImage.src = dataURI;
+	}
+
+	
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Clabhouse Ring!</h1>
+
+	<h4>Canvas:</h4>
+	<canvas id="myCanvas" crossorigin="anonymous" ></canvas>
+
+	<h4>Image</h4>
+	<img id="myImage" src="" alt="">
+
+	<br>
+
+	<button id="mainButton" on:click={display}>Display!</button>
+	<button id="mainButton">Convert!</button>
+
 </main>
 
 <style>
@@ -13,6 +61,9 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+
+		display: flex;
+		flex-direction: column;
 	}
 
 	h1 {
@@ -20,6 +71,13 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	canvas {
+		border: 1px solid red;
+	}
+
+	img {
 	}
 
 	@media (min-width: 640px) {
